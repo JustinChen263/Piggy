01c2359cd094241c4a9db057bcb466664b56870e
JustinChen263 <130698390+JustinChen263@users.noreply.github.com>
2023-05-09 18:53:55 +0000

    e

--- student.py
+++ student.py
@@ -103,10 +103,7 @@ class Piggy(PiggyParent):
           time.sleep(1)
           self.stop()
           self.turn_by_deg(-90)
-          self.fwd()
-          time.sleep(1)
-          self.stop()
-          self.turn_by_deg(90)
+          
   def turn(self):
      while True:
       self.fwd()
